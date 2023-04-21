1fc343a9f619bb17b710e0a908c42cde75be174b
Jose Rinconez <rinconez_08@hotmail.com>
2023-04-20 20:33:30 -0700

    Day 4. Creating some events for the practice project

--- Pig-Game/script.js
+++ Pig-Game/script.js
@@ -3,12 +3,41 @@
 // Selecting elements
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1'); //Another and a faster way to select id elements
-const diceEl = document.querySelector('.dice');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+// Starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
-// const number = () => Math.trunc(Math.random() * 6) + 1;
+let currentScore = 0;
+
+// Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+    // 1. Generate a random dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    // console.log(dice);
+
+    // 2. Display dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`; // Changing the source of the image with the 'src' propertie
+
+    // 3. Check roll for 1: if true -> switch to nex player
+    if (dice !== 1) {
+        // Add dice to current score
+        currentScore += dice;
+        current0El.textContent = currentScore; // CHANGE LATER
+    } else {
+        // Switch to next player1
+    }
+})
+
+
 
 // console.log(number());
