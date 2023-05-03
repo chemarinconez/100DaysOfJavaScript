260531b72534f50ca69e3a8326592fc8cab7fb1f
Jose Rinconez <rinconez_08@hotmail.com>
2023-05-03 05:51:47 -0700

    Creating the objects for the products, and some of the layout

---
+++ Shopping-Cart/script.js
@@ -0,0 +1,138 @@
+'use strict';
+
+/*
+// PRODUCT TEMPLATE
+12 products (3 x 4 on big screem)
+const product = {
+    nameOfProduct: ' ',
+    referenceNumber: 000+(3digists)
+    description:
+    value:
+    discount: `x%/100`
+}
+*/
+
+// CREATING THE PRODUCTS
+
+const product1 = {
+    nameOfProduct: "Laptop Asus Vivobook",
+    referenceNumber: '000123',
+    description: `14" Laptop, Asus, SSD 256GB, RAM 16GB, IntelCore i7 3500Hz`, 
+    value: 1250,
+    discount: 0.08,
+    amount: 2,
+    available: this.amount > 0 ? true : false,
+}
+
+const product2 = {
+    nameOfProduct: "Laptop Asus Vivobook",
+    referenceNumber: '000123',
+    description: `14" Laptop, Asus, SSD 256GB, RAM 16GB, IntelCore i7 3500Hz`, 
+    value: 1250,
+    discount: 0.08,
+    amount: 2,
+    available: this.amount > 0 ? true : false,
+}
+
+const product3 = {
+    nameOfProduct: "Laptop Asus Vivobook",
+    referenceNumber: '000123',
+    description: `14" Laptop, Asus, SSD 256GB, RAM 16GB, IntelCore i7 3500Hz`, 
+    value: 1250,
+    discount: 0.08,
+    amount: 2,
+    available: this.amount > 0 ? true : false,
+}
+
+const product4 = {
+    nameOfProduct: "Laptop Asus Vivobook",
+    referenceNumber: '000123',
+    description: `14" Laptop, Asus, SSD 256GB, RAM 16GB, IntelCore i7 3500Hz`, 
+    value: 1250,
+    discount: 0.08,
+    amount: 2,
+    available: this.amount > 0 ? true : false,
+}
+
+const product5 = {
+    nameOfProduct: "Laptop Asus Vivobook",
+    referenceNumber: '000123',
+    description: `14" Laptop, Asus, SSD 256GB, RAM 16GB, IntelCore i7 3500Hz`, 
+    value: 1250,
+    discount: 0.08,
+    amount: 2,
+    available: this.amount > 0 ? true : false,
+}
+
+const product6 = {
+    nameOfProduct: "Laptop Asus Vivobook",
+    referenceNumber: '000123',
+    description: `14" Laptop, Asus, SSD 256GB, RAM 16GB, IntelCore i7 3500Hz`, 
+    value: 1250,
+    discount: 0.08,
+    amount: 2,
+    available: this.amount > 0 ? true : false,
+}
+
+const product7 = {
+    nameOfProduct: "Laptop Asus Vivobook",
+    referenceNumber: '000123',
+    description: `14" Laptop, Asus, SSD 256GB, RAM 16GB, IntelCore i7 3500Hz`, 
+    value: 1250,
+    discount: 0.08,
+    amount: 2,
+    available: this.amount > 0 ? true : false,
+}
+
+const product8 = {
+    nameOfProduct: "Laptop Asus Vivobook",
+    referenceNumber: '000123',
+    description: `14" Laptop, Asus, SSD 256GB, RAM 16GB, IntelCore i7 3500Hz`, 
+    value: 1250,
+    discount: 0.08,
+    amount: 2,
+    available: this.amount > 0 ? true : false,
+}
+
+const product9 = {
+    nameOfProduct: "Laptop Asus Vivobook",
+    referenceNumber: '000123',
+    description: `14" Laptop, Asus, SSD 256GB, RAM 16GB, IntelCore i7 3500Hz`, 
+    value: 1250,
+    discount: 0.08,
+    amount: 2,
+    available: this.amount > 0 ? true : false,
+}
+
+const product10 = {
+    nameOfProduct: "Laptop Asus Vivobook",
+    referenceNumber: '000123',
+    description: `14" Laptop, Asus, SSD 256GB, RAM 16GB, IntelCore i7 3500Hz`, 
+    value: 1250,
+    discount: 0.08,
+    amount: 2,
+    available: this.amount > 0 ? true : false,
+}
+
+const product11 = {
+    nameOfProduct: "Laptop Asus Vivobook",
+    referenceNumber: '000123',
+    description: `14" Laptop, Asus, SSD 256GB, RAM 16GB, IntelCore i7 3500Hz`, 
+    value: 1250,
+    discount: 0.08,
+    amount: 2,
+    available: this.amount > 0 ? true : false,
+}
+
+const product12 = {
+    nameOfProduct: "Laptop Asus Vivobook",
+    referenceNumber: '000123',
+    description: `14" Laptop, Asus, SSD 256GB, RAM 16GB, IntelCore i7 3500Hz`, 
+    value: 1250,
+    discount: 0.08,
+    amount: 2,
+    available: this.amount > 0 ? true : false,
+}
+
+// Creating the Shopping Cart (Initialized as an empty Array)
+const cart = [];
